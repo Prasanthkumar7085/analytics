5243d519dd4625c84c7b6bb5a9f561eda37ca264
fe-dev1 <fe-dev1@labsquire.com>
2024-03-06 15:55:15 +0530

    SC -- Components divided in over view page

---
+++ src/components/DashboardPage/Revenue/index.tsx
@@ -0,0 +1,30 @@
+import styles from "./index.module.css";
+
+const CaseType = () => {
+  return (
+    <div className={styles.casetype}>
+      <div className={styles.header}>
+        <div className={styles.headingcontainer}>
+          <div className={styles.iconcontainer}>
+            <img className={styles.icon} alt="" src="/navbar/icon.svg" />
+          </div>
+          <div className={styles.heading}>Revenue</div>
+        </div>
+        <div className={styles.datepicker}>
+          <img
+            className={styles.calendericon}
+            alt=""
+            src="/navbar/calendericon.svg"
+          />
+          <div className={styles.daterange}>
+            <div className={styles.startDate}>Start Date</div>
+            <div className={styles.div}>-</div>
+            <div className={styles.startDate}>End Date</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CaseType;
