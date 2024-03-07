57f095518258458cdfbcc1deeb5ddce88ad6ff8e
fe-dev2 <fe-dev2@labsquire.com>
2024-03-07 12:34:59 +0530

    PR- change pie chart in stats

--- src/components/DashboardPage/CaseType/index.tsx
+++ src/components/DashboardPage/CaseType/index.tsx
@@ -4,7 +4,8 @@ import { Chart } from "react-google-charts";
 import { getCaseTypesStatsAPI } from "@/services/caseTypesAPIs";
 import TanStackTableComponent from "@/components/core/Table/caseTypesTable/TableComponent";
 import formatMoney from "@/lib/Pipes/moneyFormat";
-
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 const CaseTypes = () => {
 
   const [loading, setLoading] = useState<boolean>(true)
@@ -27,21 +28,12 @@ const CaseTypes = () => {
     }
   }
 
-
-  //options for the 2nd pie chart
-  const PieChart2Options = {
-    is3D: false,
-    title: "",
-    pieHole: 0.5,
-    legend: "none"
-  }
-
   //chagedData for pie chart
   const modifyData = (array: Array<any>) => {
     if (array && array.length) {
-      let tempArray: any = [["case_type", "total_cases"]];
+      let tempArray: any = [];
       array.map((item: any) => {
-        tempArray.push([item["case_type"], item["total_cases"] ? +item["total_cases"] : 0]);
+        tempArray.push({ name: item["case_type"], y: item["total_cases"] ? +item["total_cases"] : 0 });
       });
       return tempArray
     } else return [];
@@ -87,6 +79,52 @@ const CaseTypes = () => {
       width: "220px",
     },
   ]
+
+
+  function getSubtitle() {
+    const totalNumber = 43243;
+    return `<span style="font-size: 10px">Total value</span>
+        <br>
+        <span style="font-size: 20px;">
+            <b> ${totalNumber}</b> 
+        </span>`;
+  }
+
+  // Options for the chart
+  const options = {
+    chart: {
+      type: 'pie',
+    },
+    colors: ['#ea1d22', '#00a752', '#fcf00b', '#f19213', '#00b0ea', '#f51059', '#dc79c8', '#92298f', '#2e3094', '#0071b9'],
+    subtitle: {
+      useHTML: true,
+      text: getSubtitle(),
+      floating: true,
+      verticalAlign: 'middle',
+      y: 10
+    },
+    title: {
+      text: ''
+    },
+    plotOptions: {
+      pie: {
+        innerSize: '60%', // Make it a donut chart by setting innerSize
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: false,
+          format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+        }
+      }
+    },
+    series: [{
+      name: 'Total cases',
+      colorByPoint: true,
+      data: modifyData(caseTypesStatsData)
+    }],
+
+  };
+
   //call the api for get case types count
   useEffect(() => {
     getCaseTypesStats()
@@ -114,23 +152,22 @@ const CaseTypes = () => {
           </div>
         </div>
       </div>
-      <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", height: "330px", width: "100%" }}>
+
+      <div style={{ height: "335px", width: "100%", display: "flex" }}>
         {loading ? "" :
-          <div style={{
-            width: "30%", padding: "8px"
-          }}>
-            <Chart
-              chartType="PieChart"
-              data={modifyData(caseTypesStatsData)}
-              options={PieChart2Options}
-              width={"100%"}
-              height={"330px"}
+          <div style={{ flex: "1", marginRight: "10px" }}>
+            <HighchartsReact
+              highcharts={Highcharts}
+              options={options}
+              containerProps={{ style: { height: '100%', width: '100%' } }}
             />
-          </div>}
-        <div style={{ overflow: "auto", height: "330px", width: "70%" }}>
+          </div>
+        }
+        <div style={{ flex: "1", overflow: "auto" }}>
           <TanStackTableComponent data={caseTypesStatsData} columns={columns} />
         </div>
       </div>
+
     </div >
   );
 };
