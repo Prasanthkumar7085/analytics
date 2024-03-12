e67229285efbc466e00dc7113b75b712ee3213d7
fe-dev1 <fe-dev1@labsquire.com>
2024-03-12 19:53:14 +0530

    SC -- Update in data range pickers in dev

--- src/components/DashboardPage/Revenue/index.tsx
+++ src/components/DashboardPage/Revenue/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import RevenueDataGraph from "./RevenueDataGraph";
 import styles from "./index.module.css";
+import GlobalDateRangeFilter from "@/components/core/GlobalDateRangeFilter";
 
 const RevenueBlock = () => {
   return (
@@ -8,23 +9,17 @@ const RevenueBlock = () => {
       <div className={styles.header}>
         <div className={styles.headingcontainer}>
           <div className={styles.iconcontainer}>
-          <Image className={styles.icon} alt="" src="/navbar/icon.svg" height={20} width={20} />
+            <Image
+              className={styles.icon}
+              alt=""
+              src="/navbar/icon.svg"
+              height={20}
+              width={20}
+            />
           </div>
           <div className={styles.heading}>Revenue</div>
         </div>
-        <div className={styles.datepicker}>
-          <Image
-            className={styles.calendericon}
-            alt=""
-            src="/navbar/calendericon.svg"
-            height={20} width={20}
-          />
-          <div className={styles.daterange}>
-            <div className={styles.startDate}>Start Date</div>
-            <div className={styles.div}>-</div>
-            <div className={styles.startDate}>End Date</div>
-          </div>
-        </div>
+        <GlobalDateRangeFilter onChange={() => {}} />
       </div>
       <div style={{ width: "98%" }}>
         <RevenueDataGraph />
--- src/components/DashboardPage/SalesRep/index.tsx
+++ src/components/DashboardPage/SalesRep/index.tsx
@@ -4,34 +4,27 @@ import styles from "./index.module.css";
 import { useEffect } from "react";
 import { toast } from "sonner";
 import Image from "next/image";
+import GlobalDateRangeFilter from "@/components/core/GlobalDateRangeFilter";
 
 const SalesRep = () => {
-
-
   return (
-    <div className={styles.salesrepresentative} >
+    <div className={styles.salesrepresentative}>
       <div className={styles.header}>
         <div className={styles.headingcontainer}>
           <div className={styles.iconcontainer}>
-          <Image className={styles.icon} alt="" src="/navbar/icon.svg" height={20} width={20} />
+            <Image
+              className={styles.icon}
+              alt=""
+              src="/navbar/icon.svg"
+              height={20}
+              width={20}
+            />
           </div>
           <div className={styles.headinglable}>
             <div className={styles.heading}>Sales Representative</div>
           </div>
         </div>
-        <div className={styles.datepicker}>
-          <Image
-            className={styles.calendericon}
-            alt=""
-            src="/navbar/calendericon.svg"
-            height={20} width={20}
-          />
-          <div className={styles.daterange}>
-            <div className={styles.startDate}>Start Date</div>
-            <div className={styles.div}>-</div>
-            <div className={styles.startDate}>End Date</div>
-          </div>
-        </div>
+        <GlobalDateRangeFilter onChange={() => {}} />
       </div>
       <SalesRepsTable />
     </div>
