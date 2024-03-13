85426063e505132fd808c1afbe523eb13481a77c
Manikanta M <manikanta.m@labsquire.com>
2024-03-13 13:56:35 +0530

    Table Fixed Header and footer UI

--- src/components/core/Table/MultitpleColumn/MultipleColumnsTable.tsx
+++ src/components/core/Table/MultitpleColumn/MultipleColumnsTable.tsx
@@ -64,159 +64,151 @@ const MultipleColumnsTable: FC<pageProps> = ({
   };
 
   return (
-    <div className={styles.customTable}>
-      <div style={{ maxHeight: "calc(100vh - 200px)", overflowY: "auto" }}>
-        <table
-          style={{ borderSpacing: "0", width: "100%", tableLayout: "fixed" }}
+    <div className="tableContainer">
+      <table>
+        <thead
+          className="thead"
+          style={{
+            height: "32px",
+            position: "sticky",
+            top: "0px",
+            zIndex: "2",
+            color: "white",
+          }}
         >
-          <thead
-            className="thead"
-            style={{
-              height: "32px",
-              position: "sticky",
-              top: "0px",
-              zIndex: "2",
-              color: "white",
-            }}
-          >
-            {table
-              .getHeaderGroups()
-              .map((headerGroup: any, mainIndex: number) => (
-                <tr className="table-row" key={headerGroup.id}>
-                  {headerGroup.headers.map((header: any, index: number) => {
-                    return (
-                      <th
-                        className="cell"
-                        key={index}
-                        colSpan={header.colSpan}
-                        style={{
-                          minWidth: getWidth(header.id),
-                          width: getWidth(header.id),
-                          color: "#000",
-                          background: "#F0EDFF",
-                        }}
-                      >
-                        {header.isPlaceholder ? null : (
-                          <div
-                            // onClick={() => sortAndGetData(header)}
-                            {...{
-                              className: header.column.getCanSort()
-                                ? "cursor-pointer select-none"
-                                : "",
-                              onClick: header.column.getToggleSortingHandler(),
-                            }}
-                            style={{
-                              display: "flex",
-                              gap: "10px",
-                              cursor: "pointer",
-                              minWidth: getWidth(header.id),
-                              width: getWidth(header.id),
-                            }}
-                          >
-                            {flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                            {{
-                              asc: (
-                                <Image
-                                  src="/core/sort/sort-asc.svg"
-                                  height={15}
-                                  width={15}
-                                  alt="image"
-                                />
-                              ),
-                              desc: (
-                                <Image
-                                  src="/core/sort/sort-desc.svg"
-                                  height={15}
-                                  width={15}
-                                  alt="image"
-                                />
-                              ),
-                            }[header.column.getIsSorted() as string] ?? null}
-                          </div>
-                        )}
-                      </th>
-                    );
-                  })}
-                </tr>
-              ))}
-          </thead>
-          <tbody className="tbody">
-            {data?.length ? (
-              table.getRowModel().rows.map((row: any, mainIndex: number) => {
-                return (
-                  <tr className="table-row" key={mainIndex}>
-                    {row.getVisibleCells().map((cell: any, index: number) => {
-                      return (
-                        <td
-                          className={styles.tableCell}
-                          key={index}
-                          style={{
-                            backgroundColor: row?.original.hasOwnProperty(
-                              "target_reached"
-                            )
-                              ? !row?.original?.target_reached
-                                ? "#ffebe9"
-                                : ""
+          {table
+            .getHeaderGroups()
+            .map((headerGroup: any, mainIndex: number) => (
+              <tr className="table-row" key={headerGroup.id}>
+                {headerGroup.headers.map((header: any, index: number) => {
+                  return (
+                    <th
+                      className="cell"
+                      key={index}
+                      colSpan={header.colSpan}
+                      style={{
+                        minWidth: getWidth(header.id),
+                        width: getWidth(header.id),
+                        color: "#000",
+                        background: "#F0EDFF",
+                      }}
+                    >
+                      {header.isPlaceholder ? null : (
+                        <div
+                          // onClick={() => sortAndGetData(header)}
+                          {...{
+                            className: header.column.getCanSort()
+                              ? "cursor-pointer select-none"
                               : "",
+                            onClick: header.column.getToggleSortingHandler(),
+                          }}
+                          style={{
+                            display: "flex",
+                            gap: "10px",
+                            cursor: "pointer",
+                            minWidth: getWidth(header.id),
+                            width: getWidth(header.id),
                           }}
                         >
                           {flexRender(
-                            cell.column.columnDef.cell,
-                            cell.getContext()
+                            header.column.columnDef.header,
+                            header.getContext()
                           )}
-                        </td>
-                      );
-                    })}
-                  </tr>
-                );
-              })
-            ) : !loading ? (
-              <tr>
-                <td colSpan={10}>
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      height: "40vh",
-                    }}
-                  >
-                    {/* <Image src="/" alt="" height={150} width={800} /> */}
-                    No Data
-                  </div>
-                </td>
+                          {{
+                            asc: (
+                              <Image
+                                src="/core/sort/sort-asc.svg"
+                                height={15}
+                                width={15}
+                                alt="image"
+                              />
+                            ),
+                            desc: (
+                              <Image
+                                src="/core/sort/sort-desc.svg"
+                                height={15}
+                                width={15}
+                                alt="image"
+                              />
+                            ),
+                          }[header.column.getIsSorted() as string] ?? null}
+                        </div>
+                      )}
+                    </th>
+                  );
+                })}
               </tr>
-            ) : (
-              ""
-            )}
-          </tbody>
-        </table>
-      </div>
-      <div style={{ height: "100px", overflowY: "auto" }}>
-        <table
-          style={{ borderSpacing: "0", width: "100%", tableLayout: "fixed" }}
-        >
-          <tfoot
-            style={{
-              fontSize: "clamp(12px, 0.62vw, 14px)",
-              border: "1px solid #a5a5a5",
-              textTransform: "uppercase",
-              fontWeight: "600",
-              color: "#1B2459",
-              background: "#EFF1FA",
-            }}
-          >
+            ))}
+        </thead>
+        <tbody className="tbody">
+          {data?.length ? (
+            table.getRowModel().rows.map((row: any, mainIndex: number) => {
+              return (
+                <tr className="table-row" key={mainIndex}>
+                  {row.getVisibleCells().map((cell: any, index: number) => {
+                    return (
+                      <td
+                        className={styles.tableCell}
+                        key={index}
+                        style={{
+                          backgroundColor: row?.original.hasOwnProperty(
+                            "target_reached"
+                          )
+                            ? !row?.original?.target_reached
+                              ? "#ffebe9"
+                              : ""
+                            : "",
+                        }}
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })
+          ) : !loading ? (
             <tr>
-              {totalSumValues?.map((item: any, index: number) => {
-                return <td key={index}>{item}</td>;
-              })}
+              <td colSpan={10}>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "40vh",
+                  }}
+                >
+                  {/* <Image src="/" alt="" height={150} width={800} /> */}
+                  No Data
+                </div>
+              </td>
             </tr>
-          </tfoot>
-        </table>
-      </div>
+          ) : (
+            ""
+          )}
+        </tbody>
+        <tfoot
+          style={{
+            fontSize: "clamp(12px, 0.62vw, 14px)",
+            border: "1px solid #a5a5a5",
+            textTransform: "uppercase",
+            fontWeight: "600",
+            color: "#1B2459",
+            background: "#EFF1FA",
+          }}
+        >
+          <tr>
+            {totalSumValues?.map((item: any, index: number) => {
+              return <td key={index}>{item}</td>;
+            })}
+            <td></td>
+            <td></td>
+          </tr>
+        </tfoot>
+      </table>
     </div>
   );
 };
