bf2ca50bbeb760d6c21987b6d717644813b2d687
Manikanta M <manikanta.m@labsquire.com>
2024-03-14 22:16:57 +0530

    Login page UI

--- src/components/Auth/Signin/index.tsx
+++ src/components/Auth/Signin/index.tsx
@@ -95,87 +95,96 @@ const SignIn: NextPage = () => {
   };
   return (
     <section id="loginPage" className="flex h-screen overflow-hidden">
-      <div className="hidden md:block w-1/2 bg-indigo-900 text-white flex items-center justify-center">
-        <div className="logo">
-          <Image
-            className="w-[250px]"
-            alt=""
-            src="/auth/login/logo.svg"
-            height={20}
-            width={20}
-          />
+      <div className="hidden md:block w-1/2 bg-indigo-900 text-white signInBackground">
+        <div className="caption absolute top-[40%] left-[5%] max-w-md">
+          <h4 className="text-5xl mb-5">Welcome back!</h4>
+          <p className="text-md leading-7">
+            We are glad to see you again! Get access to your Reports, Revenues
+            and Volumes.
+          </p>
         </div>
       </div>
       <div className="w-full md:w-1/2 p-8 md:p-16 flex items-center justify-center">
         <div className="max-w-md w-full">
-          <div className="mb-8">
-            <div className="flex items-center justify-center mb-10"></div>
-
-            <h1 className="text-3xl md:text-4xl font-bold text-center text-gray-900">
-              Welcome back
-            </h1>
-            <p className="text-center text-gray-600">
-              Enter your email and password to access your account
-            </p>
+          <div className="logo flex items-center justify-center mb-20">
+            <Image
+              className="w-[250px]"
+              alt=""
+              src="/auth/login/logo.svg"
+              height={20}
+              width={20}
+            />
           </div>
-          <form className="space-y-4" onSubmit={signIn}>
-            <div>
+          <form onSubmit={signIn}>
+            <h4 className="text-3xl mb-7">Sign In</h4>
+            <div className="formGroupItem mb-7">
               <label htmlFor="email" className="block text-gray-700">
-                Email
+                Email Address
               </label>
               <input
                 id="email"
                 name="email"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                className="mt-1 block w-full border h-[50px] rounded pl-4 mb-2"
                 placeholder="Enter email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
               <ErrorMessages errorMessages={errorMessages} keyname="username" />
             </div>
-            <div>
+            <div className="form-group mb-5">
               <label htmlFor="password" className="block text-gray-700">
                 Password
               </label>
-              <div className="relative">
-                <input
-                  type={showPassword ? "text" : "password"}
-                  id="password"
-                  name="password"
-                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 pr-10"
-                  placeholder="Enter password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <button
-                  type="button"
-                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  {showPassword ? "Hide" : "Show"}
-                </button>
-              </div>
-              <ErrorMessages errorMessages={errorMessages} keyname="password" />
-              {invalidMessage && (
-                <p className="text-red-500">{invalidMessage}</p>
-              )}
-              <div className="text-right">
-                <button
-                  type="button"
-                  className="text-sm text-indigo-600 hover:underline"
-                >
-                  Forgot Password?
-                </button>
+              <div>
+                <div>
+                  <div className="relative">
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      id="password"
+                      name="password"
+                      className="mt-1 block w-full border h-[50px] rounded pl-4 mb-2"
+                      placeholder="Enter password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button
+                      type="button"
+                      className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? "Hide" : "Show"}
+                    </button>
+                  </div>
+
+                  <div className="flex justify-between items-center">
+                    <div className="w-1/2">
+                      <ErrorMessages
+                        errorMessages={errorMessages}
+                        keyname="password"
+                      />
+                    </div>
+
+                    <div className="w-1/2 forgot-password text-right">
+                      <button
+                        type="button"
+                        className="text-sm text-indigo-600 hover:underline"
+                      >
+                        Forgot Password?
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div>
-              <button
-                type="submit"
-                className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md transition duration-300 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
-              >
-                {loading ? "Loading..." : "Sign In"}
-              </button>
-            </div>
+            {invalidMessage && (
+              <p className="text-red-500 pt-3">{invalidMessage}</p>
+            )}
+            <button
+              type="submit"
+              className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md transition duration-300 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
+            >
+              {loading ? "Loading..." : "Sign In"}
+            </button>
           </form>
         </div>
       </div>
