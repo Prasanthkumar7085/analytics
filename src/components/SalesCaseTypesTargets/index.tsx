738bb63d3ed98e03745912f867c8f4575b3c5b57
fe-dev2 <fe-dev2@labsquire.com>
2024-06-01 15:46:34 +0530

    PR-add tooltips for the buttons

--- src/components/SalesCaseTypesTargets/index.tsx
+++ src/components/SalesCaseTypesTargets/index.tsx
@@ -21,7 +21,7 @@ import MultipleColumnsTableForTargets from "../core/Table/MultitpleColumn/Multip
 import { prepareURLEncodedParams } from "../utils/prepareUrlEncodedParams";
 import SalesRepsTargetsFilters from "./SalesRepsTargetsFilters";
 import BorderColorIcon from "@mui/icons-material/BorderColor";
-import { IconButton, TextField } from "@mui/material";
+import { IconButton, TextField, Tooltip } from "@mui/material";
 import SaveIcon from "@mui/icons-material/Save";
 import CloseIcon from "@mui/icons-material/Close";
 import dayjs from "dayjs";
@@ -449,49 +449,56 @@ const SalesCaseTypeWiseTargets = () => {
               {Object.keys(selectedValues)?.length &&
                 checkEditOrNot(item, info.row.original.sales_rep_id) ? (
                 <div>
-                  <IconButton
-                    sx={{ padding: "2px" }}
-                    disabled={editbleValue ? false : true}
-                    onClick={() => {
-                      updateTargets(item, info.row.original?.id);
-                    }}
-                  >
-                    <SaveIcon color="success" />
-                  </IconButton>
-                  <IconButton
-                    onClick={() => {
-                      setSelectedValues({});
-                      setUpdatedRowTotal(null);
-                      setFocusedIndex(0);
-                      setFacilityUpdatedValue(0);
-                    }}
-                    sx={{ padding: "2px" }}
-                  >
-                    <CloseIcon color="error" />
-                  </IconButton>
+                  <Tooltip title={"Save"}>
+                    <IconButton
+                      sx={{ padding: "2px" }}
+                      disabled={editbleValue ? false : true}
+                      onClick={() => {
+                        updateTargets(item, info.row.original?.id);
+                      }}
+                    >
+                      <SaveIcon color="success" />
+                    </IconButton>
+                  </Tooltip>
+
+                  <Tooltip title={"Cancel"}>
+                    <IconButton
+                      onClick={() => {
+                        setSelectedValues({});
+                        setUpdatedRowTotal(null);
+                        setFocusedIndex(0);
+                        setFacilityUpdatedValue(0);
+                      }}
+                      sx={{ padding: "2px" }}
+                    >
+                      <CloseIcon color="error" />
+                    </IconButton>
+                  </Tooltip>
                 </div>
               ) : (
-                <button
-                  disabled={dayjs().format("MM-YYYY") == params.get("month") ? false : true}
-                  onClick={() => {
-                    handleEditClick(item, info.row.original.sales_rep_id, info);
-                  }}
-                  className="editButton"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    width="24"
+                <Tooltip title={dayjs().format("MM-YYYY") == params.get("month") ? "" : "You can't edit past month targets"}>
+                  <button
+                    disabled={dayjs().format("MM-YYYY") == params.get("month") ? false : true}
+                    onClick={() => {
+                      handleEditClick(item, info.row.original.sales_rep_id, info);
+                    }}
+                    className="editButton"
                   >
-                    <path d="M0 0h24v24H0z" fill="none" />
-                    <path
-                      d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"
-                      fill="#fff"
-                    />
-                  </svg>
-                  Edit
-                </button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      width="24"
+                    >
+                      <path d="M0 0h24v24H0z" fill="none" />
+                      <path
+                        d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"
+                        fill="#fff"
+                      />
+                    </svg>
+                    Edit
+                  </button>
+                </Tooltip>
               )}
             </div>
           );
