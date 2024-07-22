f71b32697352780b11ddb882e8a590859042dc9a
code@labsquire <code@labsquire.com>
2024-07-22 19:34:42 +0530

    sw/add styles to patient results

--- src/components/PatientResults/ToxiCologyResults/ToxiResultsFilters.tsx
+++ src/components/PatientResults/ToxiCologyResults/ToxiResultsFilters.tsx
@@ -206,8 +206,9 @@ const ToxiResultsFilters = ({
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
-        justifyContent: "center",
+        justifyContent: "flex-end",
         gap: "10px",
+        flexWrap:"wrap"
       }}
     >
       <AutoCompleteForSearch
